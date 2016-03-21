1ee609c9577b541aa533c02038fde9d191e1ec46
mathiaskarl <mathiasklinke@gmail.com>
2016-03-21 10:04:14 +0100

    First commit

---
+++ Assets/Scripts/TerrainGenerator.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class TerrainGenerator : MonoBehaviour
+    {
+        public GameObject TerrainObject;
+
+        public float TerrainSizeX;
+        public float TerrainSizeY;
+
+        private Vector3 TerrainSize {  get {  return Camera.main.ScreenToWorldPoint(new Vector3(this.TerrainSizeX, this.TerrainSizeY));} }
+        private Vector3 _currentTerrainSize = new Vector3(0, 0);
+        private bool _isTerrainComplete = false;
+	
+        void Update ()
+        {
+            if(!this._isTerrainComplete)
+                GenerateTerrainOnUpdate();
+        }
+
+        public void GenerateSpecificTerrainObject(Vector3 pointFrom, Vector3 pointTo)
+        {
+            TerrainObject.gameObject.transform.localScale = new Vector3(TerrainObject.gameObject.transform.localScale.x, pointTo.y-pointFrom.y);
+            var newTerrainObject = Instantiate(TerrainObject, new Vector3(pointFrom.x, 0 + (TerrainObject.gameObject.transform.localScale.y / 2) + pointFrom.y, this.transform.position.z), Quaternion.identity) as GameObject;
+            Util.AssignObjectParent("Terrain", newTerrainObject);
+        }
+
+        private void GenerateTerrainOnUpdate()
+        {
+            for (int i = 0; i < 50; i++)
+            {
+                if (!(this._currentTerrainSize.x < this.TerrainSize.x))
+                {
+                    this._isTerrainComplete = true;
+                    return;
+                }
+
+                TerrainObject.gameObject.transform.localScale = AssignTerrainObjectSize();
+                this._currentTerrainSize.x += TerrainObject.gameObject.transform.localScale.x;
+                var newTerrainObject = Instantiate(TerrainObject, new Vector3(this._currentTerrainSize.x, 0 + TerrainObject.gameObject.transform.localScale.y / 2, this.transform.position.z), Quaternion.identity) as GameObject;
+                Util.AssignObjectParent("Terrain", newTerrainObject);
+            }
+        }
+
+        private Vector3 AssignTerrainObjectSize()
+        {
+            var x = TerrainObject.gameObject.transform.localScale.x;
+            var y = this.TerrainSize.y *
+                    Mathf.PerlinNoise(_currentTerrainSize.x * 0.01f,
+                        Mathf.Sqrt(_currentTerrainSize.x < 0 ? _currentTerrainSize.x*-1 : _currentTerrainSize.x));
+
+            return new Vector3(x, y);
+        }
+    }
+}
---
+++ Assets/Util.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets
+{
+    public class Util
+    {
+        public static void AssignObjectParent(GameObject gameObject, string childTag)
+        {
+            var objects = GameObject.FindGameObjectsWithTag(childTag);
+            foreach (var obj in objects)
+                obj.transform.parent = gameObject.transform;
+        }
+
+        public static void AssignObjectParent(string parentTag, GameObject childGameObject)
+        {
+            var parent = GameObject.FindGameObjectWithTag(parentTag);
+            childGameObject.transform.parent = parent.transform;
+        }
+
+        public static void AssignObjectParent(string parentTag, string childTag)
+        {
+            var parent = GameObject.FindGameObjectWithTag(parentTag);
+            var objects = GameObject.FindGameObjectsWithTag(childTag);
+            foreach (var obj in objects)
+                obj.transform.parent = parent.transform;
+        }
+    }
+}
