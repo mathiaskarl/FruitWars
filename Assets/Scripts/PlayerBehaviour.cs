8a6047705f466c62c01ed130691094295544e27b
JakobGrosen <sunfolk88@gmail.com>
2016-03-21 11:05:42 +0100

    Tilføjet midlertidig spiller + controls

---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class PlayerBehaviour : MonoBehaviour
+{
+
+    public float MovementSpeed;
+
+	// Use this for initialization
+	void Start ()
+	{
+
+	}
+
+    // Update is called once per frame
+    private void Update()
+    {
+        MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
+        transform.position += new Vector3(
+            Input.GetAxis("Horizontal") * Time.deltaTime * MovementSpeed,
+            Input.GetAxis("Vertical") * Time.deltaTime * MovementSpeed, 0);
+    }
+}
